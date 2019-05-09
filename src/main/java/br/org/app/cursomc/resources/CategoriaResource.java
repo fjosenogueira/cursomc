105ecbac22458f89906699e1b4170193634daba0
Francisco J Nogueira <fjosenogueira@gmail.com>
2019-05-09 15:51:16 -0300

    Nome da classe CategoriaResource alterada

--- src/main/java/br/org/app/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/org/app/cursomc/resources/CategoriaResource.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/categorias")
-public class CategoriaResources {
+public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public String listar() {
